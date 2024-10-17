c3e65daa4eb639918db58a93769cd863e744c73f
Докс <nox23061992@gmail.com>
2024-10-17 11:47:22 +0300

    Сделали графический интерфейс: добавили место для вывода картинки и кнопку

---
+++ DogsJSON.py
@@ -0,0 +1,17 @@
+import requests
+from tkinter import *
+from tkinter import ttk
+from PIL import Image, ImageTk
+from io import BytesIO
+
+window = Tk()
+window.geometry('500x400')
+window.title('Изображения собак')
+
+i_m = Label()
+i_m.pack()
+
+btn = Button(text='Получить собачку')
+btn.pack()
+
+window.mainloop()
