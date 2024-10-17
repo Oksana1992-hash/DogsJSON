1fdabdd38d28861b072f0fb9bdd76415972b606d
Докс <nox23061992@gmail.com>
2024-10-17 12:02:03 +0300

    Создали функцию для получения ссылки на картинку с json "get_json_dog()"
    Создали функцию для добавления картинки в Label

--- DogsJSON.py
+++ DogsJSON.py
@@ -4,6 +4,24 @@ from tkinter import ttk
 from PIL import Image, ImageTk
 from io import BytesIO
 
+
+def get_json_dog():
+    answer_api = requests.get('https://dog.ceo/api/breeds/image/random')
+    json_dog = answer_api.json()
+    return json_dog['message']
+
+
+def image_dog_in_tk():
+    url_image = get_json_dog()
+    if url_image:
+        answer_img = requests.get(url_image)
+        image = BytesIO(answer_img.content)
+        img = Image.open(image)
+        img.thumbnail((500, 350))
+        img_tk = ImageTk.PhotoImage(img)
+        i_m.config(image=img_tk)
+        i_m.image = img_tk
+
 window = Tk()
 window.geometry('500x400')
 window.title('Изображения собак')
@@ -11,7 +29,7 @@ window.title('Изображения собак')
 i_m = Label()
 i_m.pack()
 
-btn = Button(text='Получить собачку')
+btn = Button(text='Получить собачку', command=image_dog_in_tk)
 btn.pack()
 
 window.mainloop()
